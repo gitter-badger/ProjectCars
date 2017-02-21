5f896b361b5494d04bff790e06b884c42d3c9ad7
RinatRiyanov <correon39@gmail.com>
2017-02-22 01:06:50 +0300

    add gulp and postcss

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+
+gulp.task('styles', function() {
+  return gulp.src('./src/styles/main.css')
+    .pipe(postcss([autoprefixer]))
+    .pipe(gulp.dest('./build/styles'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/styles/main.css', ['styles']);
+});
