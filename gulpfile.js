33c3454420fb9422bb623ac4c94decd23065e3ad
Savina Anastasia <nastya.savina.nn@gmail.com>
2017-02-24 00:52:55 +0700

    added sourcemaps and concat to gulp.task('styles'); change margin for button

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
 
 gulp.task('styles', function() {
-  return gulp.src('./src/styles/main.css')
+  return gulp.src('./src/styles/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(concat('all.css'))
     .pipe(postcss([autoprefixer]))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./build/styles'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./src/styles/main.css', ['styles']);
+  gulp.watch('./src/styles/*.css', ['styles']);
 });
+
+gulp.task('default', ['styles','watch']);
