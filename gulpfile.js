84928b39df0ab23adac39f1f036da10b71e4ad92
Savina Anastasia <nastya.savina.nn@gmail.com>
2017-02-25 19:55:34 +0700

    connected postcss-assets, postcss-nested, postcss-short (they working); connected stylelint and postcss-reporter (they didn't working)

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,28 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
+var assets = require('postcss-assets');
+var nested = require('postcss-nested');
+var short = require('postcss-short');
+var stylelint = require('stylelint');
+var reporter = require('postcss-reporter');
+
+var plugins = [
+      autoprefixer({
+        browsers: ['last 2 versions', '> 2%']
+      }),
+      assets({
+       loadPaths: ['src/img/', 'src/fonts/ProximaNovaBold', 'src/fonts/ProximaNovaLight', 'src/fonts/ProximaNovaRegular']
+      }),
+      nested(),
+      short()
+  ];
 
 gulp.task('styles', function() {
   return gulp.src('./src/styles/*.css')
     .pipe(sourcemaps.init())
     .pipe(concat('all.css'))
-    .pipe(postcss([autoprefixer({})]))
+    .pipe(postcss(plugins))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./build/styles'))
 });
