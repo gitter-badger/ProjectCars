03e943dac1553040232b0004b4d8570da47c2490
Savina Anastasia <nastya.savina.nn@gmail.com>
2017-02-27 23:44:38 +0700

    dleted second .pipe(postcss())

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,30 @@ var short = require('postcss-short');
 var stylelint = require('stylelint');
 var reporter = require('postcss-browser-reporter');
 
-var plugins = [
-      autoprefixer({
-        browsers: ['last 2 versions', '> 2%']
-      }),
-      assets({
-       loadPaths: ['src/img/', 'src/fonts/ProximaNovaBold', 'src/fonts/ProximaNovaLight', 'src/fonts/ProximaNovaRegular']
-      }),
-      nested(),
-      short(),
-];
-
 var lint = [
+  autoprefixer({
+    browsers: ['last 2 versions', '> 2%']
+  }),
+  assets({
+    loadPaths: ['src/img/',
+    'src/fonts/ProximaNovaBold',
+    'src/fonts/ProximaNovaLight',
+    'src/fonts/ProximaNovaRegular']
+  }),
+  nested(),
   stylelint({
     "extends": "stylelint-config-standard",
     "rules": {}
   }),
-  reporter()
+  reporter(),
+  short()
 ]
-
 
 gulp.task('styles', function() {
   return gulp.src('./src/styles/*.css')
-    .pipe(postcss(lint))
     .pipe(sourcemaps.init())
+    .pipe(postcss(lint))
     .pipe(concat('all.css'))
-    .pipe(postcss(plugins))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./build/styles'))
 });
