6c4ea0892e66910b60d7ca76310af93b80ae9353
RinatRiyanov <correon39@gmail.com>
2017-03-01 03:10:14 +0300

    add-functions-and-objects

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -10,64 +10,107 @@ var calculateModule = (function () {
     button.addEventListener('click', _calculate)
   };
 
+  var _newTiresData = {
+    diameters: {
+      inner: 0,
+      outer: 0
+    },
+    width: 0,
+    sidewall: 0,
+    height: 0,
+    difference: {
+      diameters: {
+        inner: 0,
+        outer: 0
+      },
+      width: 0,
+      height: 0
+    },
+    clearance: 0,
+    speedometer: 0
+  };
+
+  var _oldTiresData = {
+    diameters: {
+      inner: 0,
+      outer: 0
+    },
+    width: 0,
+    sidewall: 0,
+    height: 0,
+    speedometer: 0
+  };
+
   var _calculate = function () {
 
     var _oldWidthSelect = document.getElementById('oldWidthSelect');
-    var _oldHeightSelect = document.getElementById('oldHeightSelect');
+    var _oldSidewallSelect = document.getElementById('oldSidewallSelect');
     var _oldInnerDiameterSelect = document.getElementById('oldInnerDiameterSelect');
 
     var _newWidthSelect = document.getElementById('newWidthSelect');
-    var _newHeightSelect = document.getElementById('newHeightSelect');
+    var _newSidewallSelect = document.getElementById('newSidewallSelect');
     var _newInnerDiameterSelect = document.getElementById('newInnerDiameterSelect');
 
-    var _oldHeightSelectValue = +_oldHeightSelect.options[_oldHeightSelect.selectedIndex].value;
-    var _newHeightSelectValue = +_newHeightSelect.options[_newHeightSelect.selectedIndex].value;
+    var _oldWidth = document.getElementById('oldWidth');
+    var _newWidth = document.getElementById('newWidth');
+    var _differenceWidth = document.getElementById('differenceWidth');
 
-    var _oldWidthOutput = document.getElementById('oldWidthOutput');
-    var _newWidthOutput = document.getElementById('newWidthOutput');
-    var _differenceWidthOutput = document.getElementById('differenceWidthOutput');
+    var _oldSidewall = _oldSidewallSelect.options[_oldSidewallSelect.selectedIndex].value;
+    var _newSidewall = _newSidewallSelect.options[_newSidewallSelect.selectedIndex].value;
 
-    var _oldInnerDiameterOutput = document.getElementById('oldInnerDiameterOutput');
-    var _newInnerDiameterOutput = document.getElementById('newInnerDiameterOutput');
-    var _differenceInnerDiameterOutput = document.getElementById('differenceInnerDiameterOutput');
+    var _oldInnerDiameter = document.getElementById('oldInnerDiameter');
+    var _newInnerDiameter = document.getElementById('newInnerDiameter');
+    var _differenceInnerDiameter = document.getElementById('differenceInnerDiameter');
 
-    var _oldOuterDiameterOutput = document.getElementById('oldOuterDiameterOutput');
-    var _newOuterDiameterOutput = document.getElementById('newOuterDiameterOutput');
-    var _differenceOuterDiameterOutput = document.getElementById('differenceOuterDiameterOutput');
+    var _oldOuterDiameter = document.getElementById('oldOuterDiameter');
+    var _newOuterDiameter = document.getElementById('newOuterDiameter');
+    var _differenceOuterDiameter = document.getElementById('differenceOuterDiameter');
 
-    var _oldHeightOutput = document.getElementById('oldHeightOutput');
-    var _newHeightOutput = document.getElementById('newHeightOutput');
-    var _differenceHeightOutput = document.getElementById('differenceHeightOutput');
+    var _oldHeight = document.getElementById('oldHeight');
+    var _newHeight = document.getElementById('newHeight');
+    var _differenceHeight = document.getElementById('differenceHeight');
 
     var _clearance = document.getElementById('clearance');
 
-    var _speedometerValue = document.getElementById('speedometerValue');
-    var _speedometerTrueValue = document.getElementById('speedometerTrueValue');
+    var _speedometer = document.getElementById('speedometer');
+    var _speedometerTrue = document.getElementById('speedometerTrue');
     var _speedometerDifference = document.getElementById('speedometerDifference');
 
-    _oldWidthOutput.value = _oldWidthSelect.options[_oldWidthSelect.selectedIndex].value;
-    _newWidthOutput.value = _newWidthSelect.options[_newWidthSelect.selectedIndex].value;
-    _differenceWidthOutput.value = _oldWidthOutput.value - _newWidthOutput.value;
+    _oldWidth.value = _oldWidthSelect.options[_oldWidthSelect.selectedIndex].value;
+    _newWidth.value = _newWidthSelect.options[_newWidthSelect.selectedIndex].value;
+    _differenceWidth.value = _oldWidth.value - _newWidth.value;
 
-    _oldInnerDiameterOutput.value = Math.round(_oldInnerDiameterSelect.options[_oldInnerDiameterSelect.selectedIndex].value * 25.4);
-    _newInnerDiameterOutput.value = Math.round(_newInnerDiameterSelect.options[_newInnerDiameterSelect.selectedIndex].value * 25.4);
-    _differenceInnerDiameterOutput.value = _oldInnerDiameterOutput.value - _newInnerDiameterOutput.value;
+    _oldInnerDiameter.value = _getInnerDiameter(_oldInnerDiameterSelect);
+    _newInnerDiameter.value = _getInnerDiameter(_newInnerDiameterSelect);
+    _differenceInnerDiameter.value = _oldInnerDiameter.value - _newInnerDiameter.value;
 
-    _oldOuterDiameterOutput.value = +_oldWidthOutput.value + +_oldInnerDiameterOutput.value;
-    _newOuterDiameterOutput.value = +_newWidthOutput.value + +_newInnerDiameterOutput.value;
-    _differenceOuterDiameterOutput.value = +_differenceWidthOutput.value + +_differenceInnerDiameterOutput.value;
+    _oldOuterDiameter.value = _getOuterDiameter(_oldWidth, _oldInnerDiameter);
+    _newOuterDiameter.value = _getOuterDiameter(_newWidth, _newInnerDiameter);
+    _differenceOuterDiameter.value = _oldOuterDiameter.value - _newOuterDiameter.value;
 
-    _oldHeightOutput.value = (_oldOuterDiameterOutput.value - _oldInnerDiameterOutput.value) / 2;
-    _newHeightOutput.value = (_newOuterDiameterOutput.value - _newInnerDiameterOutput.value) / 2;
-    _differenceHeightOutput.value = (_differenceInnerDiameterOutput.value - _differenceOuterDiameterOutput.value) / 2;
+    _oldHeight.value = _getHeight(_oldOuterDiameter, _oldInnerDiameter);
+    _newHeight.value = _getHeight(_newOuterDiameter, _newInnerDiameter);
+    _differenceHeight.value = _oldHeight.value - _newHeight.value;
 
-    _clearance.value = _oldOuterDiameterOutput.value - _newOuterDiameterOutput.value;
+    _clearance.value = _oldOuterDiameter.value - _newOuterDiameter.value;
 
-    _speedometerTrueValue.value = Math.round(_speedometerValue.value / ((_oldWidthOutput.value * (_oldHeightSelectValue / 100) + _oldInnerDiameterOutput.value) / (_newWidthOutput.value * (_newHeightSelectValue / 100) + _newInnerDiameterOutput.value)));
+    _speedometerTrue.value = Math.round(_speedometer.value / ((_oldWidth.value * (_oldSidewall / 100) + Number(_oldInnerDiameter.value)) / (_newWidth.value * (_newSidewall / 100) + Number(_newInnerDiameter.value))));
 
-    _speedometerDifference.value = _speedometerValue.value - _speedometerTrueValue.value;
+    _speedometerDifference.value = _speedometer.value - _speedometerTrue.value;
 
   };
+
+  function _getInnerDiameter(diameter) {
+    return Math.round(diameter.options[diameter.selectedIndex].value * 25.4);
+  };
+
+  function _getOuterDiameter(width, innerDiameter) {
+    return Number(width.value) + Number(innerDiameter.value);
+  };
+
+  function _getHeight(outerDiameter, innerDiameter) {
+    return (outerDiameter.value - innerDiameter.value) / 2;
+  }
 
   return {
     init: _init
