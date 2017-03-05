82798cf04ca9687b04baeded0b73317153b08051
AlenaDmit <dmitalyona@yandex.ru>
2017-03-05 16:58:33 +0300

    paths chanched, spaces deleted

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,37 @@ var concat = require('gulp-concat');
 const hbsContext = require('./src/test.json');
 
 var paths = {
-	srcDir : './src',
-	buildDir : './build',
-	hbsPartials : ['./src/partials/'],
-	styles : './src/styles/*.css'
-}
+	srcDir: './src',
+	buildDir: './build',
+	templatesDir: ['./src/partials/'],
+	templates: './src/partials/*.hbs',
+	hbsIndex: './src/index.hbs',
+	stylesSrc: './src/styles/*.css',
+	stylesBuild: './build/styles'
+};
 
 gulp.task('default', ['styles', 'watch', 'handlebars']);
 
 gulp.task('styles', function() {
-  return gulp.src(paths.styles)
+  return gulp.src(paths.stylesSrc)
     .pipe(sourcemaps.init())
     .pipe(concat('style.css'))
     .pipe(postcss([autoprefixer]))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.buildDir + '/styles'))
+    .pipe(gulp.dest(paths.stylesBuild))
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.stylesSrc, ['styles']);
+  gulp.watch(paths.templates, ['handlebars']);
 });
 
 gulp.task('handlebars', function () {
 	var options = {
-		batch: paths.hbsPartials
+		batch: paths.templatesDir
 	};
-	return gulp.src(paths.srcDir + '/index.hbs')
+
+	return gulp.src(paths.hbsIndex)
 		.pipe(handlebars(hbsContext, options))
 		.pipe(rename('index.html'))
 		.pipe(gulp.dest(paths.buildDir));
